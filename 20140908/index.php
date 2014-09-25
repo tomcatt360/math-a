d7599746bf0834931b79bdef4b00270f149c3f75
Thomas Parker <tomcatt360@gmail.com>
2014-09-25 06:36:45 -0500

    Fixed tab indentation in 20140908/index.php
    PHP if statments had needed indentation to increase code readability.

--- 20140908/index.php
+++ 20140908/index.php
@@ -7,45 +7,44 @@
 			<h1>Temperature Converter</h1><hr>
 			<!--If $_GET[temp] == null then:-->
 			<?php if($_GET["temp"] == NULL) { ?>
-			<!--Begin form to collect temperature and conversion type from user.-->
-			<form action="index.php" Method="get">
-				Input temperature that you would like to convert: <input type="number" name="temp">
-				<select name="unit">
-					<option value="f">F -<code>></code> C</option>
-					<option value="c">C -<code>></code> F</option>
-				</select>
-				<br>
-				<input type="submit">
-			</form>
-			<!--End form to collect temperature and conversion type from user.-->
-			<!--Else:-->
+				<!--Begin form to collect temperature and conversion type from user.-->
+				<form action="index.php" Method="get">
+					Input temperature that you would like to convert: <input type="number" name="temp">
+					<select name="unit">
+						<option value="f">F -<code>></code> C</option>
+						<option value="c">C -<code>></code> F</option>
+					</select>
+					<br>
+					<input type="submit">
+				</form>
+				<!--End form to collect temperature and conversion type from user.-->
+				<!--Else:-->
 			<?php
 				}
 				else{
-				if($_GET["unit"] == "f"){
+					if($_GET["unit"] == "f"){
 			?>
-			<!--Begin processing and display output F -> C-->
-			<p>
-				<?php
-					$f_2_c=($_GET["temp"]-32)*5/9;
-				?>
-				Celsius: <?=$f_2_c?>
-			</p>
-			<!--End processing F -> C-->
-			<?php }else{ ?>
-			<!--Begin processing C -> F-->
-			<p>
-				<?php
-					$c_2_f=$_GET["temp"]*9/5+32;
-				?>
-				Fahrenheit:<?=$c_2_f?>
-			</p>
-			<!--End processing C -> F-->
-			<?php } ?>
-			<p><a href="http://208.94.240.5/20140908">Convert Again</a></p>
+					<!--Begin processing and display output F -> C-->
+					<p>
+					<?php
+						$f_2_c=($_GET["temp"]-32)*5/9;
+					?>
+					Celsius: <?=$f_2_c?>
+				</p>
+				<!--End processing F -> C-->
+				<?php }else{ ?>
+				<!--Begin processing C -> F-->
+				<p>
+					<?php
+						$c_2_f=$_GET["temp"]*9/5+32;
+					?>
+					Fahrenheit:<?=$c_2_f?>
+				</p>
+				<!--End processing C -> F-->
+				<?php } ?>
+				<p><a href="http://208.94.240.5/20140908">Convert Again</a></p>
 			<?php } ?>
 			<p><a href="http://208.94.240.5">Home</a></p>
-
 		</center>
 		<p><a href="index.txt">View Source</a></p>
 	</body>
