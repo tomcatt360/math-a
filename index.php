7f60bafe812720ef218b295922f1248413d15ee0
Thomas Parker <tomcatt360@gmail.com>
2014-09-25 06:32:21 -0500

    Changed 'In Construction' To 'NEW!' in home page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 			<p>
 				<a href="./20140822/">20140822 - Hello World</a><br>
 				<a href="./20140826/">20140826 - Favorite Food Form</a><br>
-				<a href="./20140908/">20140826 - Temperature Converter</a><b> In Construction! ;)</b>
+				<a href="./20140908/">20140826 - Temperature Converter</a><b> NEW!</b>
 			</p>
 		</center>
 		<p><a href="index.txt">View Source</a></p>
