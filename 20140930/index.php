2bcadf204c2696e5611d11f2b7b97a6f5647526a
Thomas Parker <tomcatt360@gmail.com>
2014-10-13 15:50:41 +0000

    fixme index.php

--- 20140930/index.php
+++ 20140930/index.php
@@ -2,5 +2,13 @@
 include './header.php'
 
 echo "<h1>tomcatt's number comparator<h1>"
+
+$number1=$_GET[number1]
+$number2=$_GET[number2]
+if ($number1 != null && $number2 != null) {
+	if (is_numeric($number1) && is_numeric($number2)){
+		
+	}
+}
 include './footer.php'
 ?>
