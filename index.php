a56916d53c906b64999d0ca9bb0bc40ec174035d
Thomas Parker <tomcatt360@gmail.com>
2014-10-20 06:45:32 -0500

    fixed wrong link names

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 			<p>
 				<a href="./20140822/">20140822 - Hello World</a><br>
 				<a href="./20140826/">20140826 - Favorite Food Form</a><br>
-				<a href="./20140908/">20140826 - Temperature Converter</a><br>
-				<a href="./20140930/">20140826 - Two-Number Comparator</a><b>In Construction</b><br>
-				<a href="./20141014/">20140914 - Three-Number Comparator</a><b>In Construction</b><br>
+				<a href="./20140908/">20140908 - Temperature Converter</a><br>
+				<a href="./20140930/">20140830 - Two-Number Comparator</a><b>In Construction</b><br>
+				<a href="./20141014/">20141014 - Three-Number Comparator</a><b>In Construction</b><br>
 			</p>
 		</center>
 		<p><a href="index.txt">View Source</a></p>
