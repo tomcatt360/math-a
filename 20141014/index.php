0807c82bc24b0402d9b7e4fadffdedd90defe264
Thomas Parker <tomcatt360@gmail.com>
2014-10-22 06:49:59 -0500

    Fixed comparisons where number pairs were equal

--- 20141014/index.php
+++ 20141014/index.php
@@ -9,7 +9,6 @@ $number3=$_GET[number3];
 if ($number1 != null && $number2 != null && $number3 != null) {
 	if (is_numeric($number1) && is_numeric($number2) && is_numeric($number3)){
 		// If the numbers are not null, and the numbers are truly numbers, Process input.
-		// FIXME Add more comparisons
 		
 		if ($number1 == $number2){
 			if ($number1 == $number3){
@@ -20,23 +19,23 @@ if ($number1 != null && $number2 != null && $number3 != null) {
 				echo "Number 1 and Number 2 are equal, Number 3 is greater than each of them.";
 			}
 		}elseif($number1 > $number2){
-			if ($number2 > $number3){
-				echo "Number 1 is greater than Number 2. Number 2 is greater than Number 3.";
-			}elseif ($number1 > $number3){
-				echo "Number 1 is greater than Number 3. Number 3 is greater than number 2.";
+			if ($number2 >= $number3){
+				echo "Number 1 is greater than Number 2. Number 2 is greater than or equal to Number 3.";
+			}elseif ($number1 >= $number3){
+				echo "Number 1 is greater than or equal to Number 3. Number 3 is greater than number 2.";
 			}else{
 				echo "Number 3 is greater than number 1. Number 1 is greater than number 2.";
 			}
 		}elseif ($number2 > $number1){
-			if ($number1 > $number3){
-                                echo "Number 2 is greater than Number 1. Number 1 is greater than Number 3.";
-                        }elseif ($number2 > $number3){
-                                echo "Number 2 is greater than Number 3. Number 3 is greater than number 1.";
+			if ($number1 >= $number3){
+                                echo "Number 2 is greater than Number 1. Number 1 is greater than or equal to Number 3.";
+                        }elseif ($number2 >= $number3){
+                                echo "Number 2 is greater than or equal to Number 3. Number 3 is greater than number 1.";
                         }else{
                                 echo "Number 3 is greater than number 2. Number 2 is greater than number 1.";
 			}
 		}
-		echo "<a href=\"./index.php\">Compare more numbers</a>";
+		echo "<br><a href=\"./index.php\">Compare more numbers</a>";
 	}else{
 		// User input wrong
 		echo "<h1>User Input Wrong</h1><br>
