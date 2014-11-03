2fbbbc5554693797ed0c1626285e5c5e6bf42729
Thomas Parker <tomcatt360@gmail.com>
2014-11-03 06:36:24 -0600

    Created blank for numeric sequencer

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 				<a href="./20140930/">20140830 - Two-Number Comparator</a><br>
 				<a href="./20141014/">20141014 - Three-Number Comparator</a><br>
 				<a href="./20141023/">20141023 - Three-Number Comparator v2</a><br>
+				<a href="./20141103/">20141103 - Numeric Sequencer</a><b> In Construction</b>
 			</p>
 		</center>
 		<p><a href="index.txt">View Source</a></p>
