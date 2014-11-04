42c4bb037c6f441cb842baa3a0e06ada67b741c6
Thomas Parker <tomcatt360@gmail.com>
2014-11-04 10:42:57 -0600

    Added form to allow users to enter their own range for the sequence

--- 20141103/index.php
+++ 20141103/index.php
@@ -6,11 +6,23 @@ Written by Thomas Parker<tomcatt360@gmail.com
 Creates a sequence of numbers based on the user's input
 */
 echo "<h1>Numeric Sequencer</h1>";
-$start=1;
-$stop=100;
-while ($start <= $stop){
-echo "$start<br>";
-$start = $start + 1;
+
+if ($_GET[start] != null && $_GET[stop] != null){
+
+	$start=$_GET[start];
+	$stop=$_GET[stop];
+
+	while ($start <= $stop){
+		echo "$start<br>";
+		$start = $start + 1;
+	}
+}else{
+	//user has not entered anything, show html form
+	echo "<form action=\"./index.php\" method=\"get\">
+	Starting Number: <input type=\"text\" name=\"start\"><br>
+	Ending Number: <input type \"text\" name=\"stop\"><br>
+	<input type=\"submit\" value=\"Create Sequence!\">
+	</form>";
 }
 include './footer.php';
 ?>
