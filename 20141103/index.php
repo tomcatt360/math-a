00cb3b5a5c95b53363fb5b284bc61ffbff27515b
Thomas Parker <tomcatt360@gmail.com>
2014-11-04 16:03:49 -0600

    Sanitze input for non-numeric values, various fixes.
    Fixed typo in comment at the beginning
    Added if condition to sanitize for non-numeric values.
    Cleaned up input form.
    TODO: Sanitize for a starting value that is larger than the ending value.

--- 20141103/index.php
+++ 20141103/index.php
@@ -2,26 +2,29 @@
 include './header.php';
 /*
 Numeric Sequencer
-Written by Thomas Parker<tomcatt360@gmail.com
+Written by Thomas Parker <tomcatt360@gmail.com>
 Creates a sequence of numbers based on the user's input
 */
 echo "<h1>Numeric Sequencer</h1>";
 
 if ($_GET[start] != null && $_GET[stop] != null){
+	if (is_numeric($_GET[start]) && is_numeric($_GET[stop])){
+		$start=$_GET[start];
+		$stop=$_GET[stop];
 
-	$start=$_GET[start];
-	$stop=$_GET[stop];
-
-	while ($start <= $stop){
-		echo "$start<br>";
-		$start = $start + 1;
+		while ($start <= $stop){
+			echo "$start<br>";
+			$start = $start + 1;
+		}
 	}
 }else{
 	//user has not entered anything, show html form
 	echo "<form action=\"./index.php\" method=\"get\">
-	Starting Number: <input type=\"text\" name=\"start\"><br>
-	Ending Number: <input type \"text\" name=\"stop\"><br>
-	<input type=\"submit\" value=\"Create Sequence!\">
+	<table>
+	<tr><td>Starting Number: </td><td><input type=\"text\" name=\"start\"></td></tr>
+	<tr><td>Ending Number: </td><td><input type \"text\" name=\"stop\"></td></tr>
+	<tr><td><input type=\"submit\" value=\"Create Sequence!\"></td><td></td><tr>
+	</table>
 	</form>";
 }
 include './footer.php';
